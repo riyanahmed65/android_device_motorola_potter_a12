3068b0e126b891f4d3d24b547e58daa1a90f7d21
Michael Bestas <mkbestas@lineageos.org>
2021-10-07 22:04:41 +0300

    potter: sepolicy: Switch to SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOL…

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -219,8 +219,8 @@ BOARD_ROOT_EXTRA_FOLDERS := persist
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-#BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Wifi
